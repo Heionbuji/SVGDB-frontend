833f8694a9192df28fd36172e7eb88e285c1fcd5
Hatred <48438453+Heionbuji@users.noreply.github.com>
2020-08-22 17:49:29 +0300

    Added class filtering and sorting

--- src/components/Deckbuilder.jsx
+++ src/components/Deckbuilder.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useEffect, useState } from 'react';
 import { withTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
@@ -13,7 +14,7 @@ const propTypes = {
 };
 
 const Deckbuilder = ({ t, i18n }) => {
-  const [selectedClass, setSelectedClass] = useState(1);
+  const [selectedClass, setSelectedClass] = useState(null);
   const [cardsJson, setCardsJson] = useState(null);
   const thumbnailUrl = `${process.env.REACT_APP_ASSETS_URL}/thumbnails/C_`;
   useEffect(() => {
@@ -24,13 +25,32 @@ const Deckbuilder = ({ t, i18n }) => {
   return (
     <Container>
       <div style={{ backgroundColor: '#555' }}>
-        Select class:
+        <span>Select class:</span>
+        <span>
+          <button type="button" onClick={() => setSelectedClass('1')}>Forest</button>
+          <button type="button" onClick={() => setSelectedClass('2')}>Sword</button>
+          <button type="button" onClick={() => setSelectedClass('3')}>Rune</button>
+          <button type="button" onClick={() => setSelectedClass('4')}>Dragon</button>
+          <button type="button" onClick={() => setSelectedClass('5')}>Shadow</button>
+          <button type="button" onClick={() => setSelectedClass('6')}>Blood</button>
+          <button type="button" onClick={() => setSelectedClass('7')}>Haven</button>
+          <button type="button" onClick={() => setSelectedClass('8')}>Portal</button>
+        </span>
       </div>
       <div style={{ display: 'flex' }}>
         <div style={{ flex: '1', margin: '20px' }}>
-          {cardsJson && Object.keys(cardsJson).map((key) => (
-            <LazyLoadedImage src={`${thumbnailUrl}${key}.png`} alt="" />
-          ))}
+          {cardsJson && selectedClass && Object.keys(cardsJson).filter((item) => {
+            const id = cardsJson[item].id_.toString();
+            return (
+              id.substring(3, 4) === selectedClass
+              && id.substring(0, 1) !== '9'
+              && id.substring(0, 1) !== '7'
+            );
+          })
+            .sort((a, b) => cardsJson[a].pp_ > cardsJson[b].pp_)
+            .map((key) => (
+              <LazyLoadedImage key={`img${key}`} src={`${thumbnailUrl}${key}.png`} alt="" />
+            ))}
         </div>
         <div style={{ width: '400px', color: 'white' }}>
           Deck and stuff
