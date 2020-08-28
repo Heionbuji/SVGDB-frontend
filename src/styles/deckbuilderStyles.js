5ccd5109a774a3e9fc349cb5648e69f8a36810cc
Hatred <48438453+Heionbuji@users.noreply.github.com>
2020-08-28 19:18:45 +0300

    Added card highlight on hover

--- src/styles/deckbuilderStyles.js
+++ src/styles/deckbuilderStyles.js
@@ -4,6 +4,16 @@ export const Container = styled.div`
   width: 95vw;
   margin: auto;
   background-color: black;
+
+  .cardhover:hover img {
+    filter: drop-shadow(0 0 7px cyan);
+  }
+  .cardhover img {
+    transition: filter .2s;
+  }
+  .cardhover:hover img {
+    cursor: pointer;
+  }
 `;
 
 export const Tooltip = styled.div`
