2576fe1e14c3df99b72fe7ca48af82bd1de0ba1f
Hatred <48438453+Heionbuji@users.noreply.github.com>
2020-09-02 11:06:38 +0300

    Added some styling for deck view

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { ManaCircle } from '../styles/deckStyles';
+
 const propTypes = {
   deck: PropTypes.arrayOf(PropTypes.string).isRequired,
   cards: PropTypes.shape({
@@ -13,8 +15,25 @@ const Deck = ({ deck, cards }) => {
   return (
     <div style={{ width: '20vw', color: 'white' }}>
       {deck && (
-        // eslint-disable-next-line react/no-array-index-key
-        Object.keys(deck).map((card, index) => <p key={`${card}+${index}`}>{`${cards[card].pp_} ${cards[card].name_} x${deck[card]}`}</p>)
+        Object.keys(deck).map((card, index) => (
+          <div
+            // eslint-disable-next-line react/no-array-index-key
+            key={`${card}+${index}`}
+            style={{ display: 'flex', justifyContent: 'space-between', paddingTop: '3px' }}
+          >
+            <ManaCircle>
+              <span style={{ textShadow: '2px 2px 2px black', marginTop: '4px' }}>
+                {cards[card].pp_}
+              </span>
+            </ManaCircle>
+            <div>
+              {cards[card].name_}
+            </div>
+            <div>
+              x{deck[card]}
+            </div>
+          </div>
+        ))
       )}
     </div>
   );
---
+++ src/styles/deckStyles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ManaCircle = styled.div`
+  background: radial-gradient(circle at 60% 40%, #33ff00, #152810 70%);
+  background-color: green;
+  border-radius: 50%;
+  width: 25px;
+  height: 25px;
+  text-align: center;
+  padding: 3px;
+`;
