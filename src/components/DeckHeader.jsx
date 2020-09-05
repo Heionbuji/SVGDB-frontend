1465631d1787b4018bf43846402501b4acdfd208
Hatred <48438453+Heionbuji@users.noreply.github.com>
2020-09-05 18:22:08 +0300

    Added deck hash generation and minor fixes

---
+++ src/components/DeckHeader.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable no-restricted-syntax */
+import React from 'react';
+
+const DeckHeader = ({ deck }) => {
+  const computeDeckHash = () => {
+    // starts with something like 1.1., first one is mode (1 = unlimi, 3 = rot)
+    // second one is class id
+    const radix = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_';
+    let ret = '1.1.';
+    Object.keys(deck).forEach((key) => {
+      let temp = '';
+      let tempKey = key;
+      while (parseInt(tempKey, 10) > 0) {
+        temp += radix[parseInt(tempKey, 10) % 64];
+        tempKey = Math.floor(parseInt(tempKey, 10) / 64);
+      }
+      ret += `${temp.split('').reverse().join('')}.`.repeat(deck[key]);
+    });
+    return (ret.substring(0, ret.length - 1));
+  };
+
+  const openInPortal = () => {
+    const hash = computeDeckHash();
+    console.log(hash);
+  };
+  return (
+    <div style={{ backgroundColor: 'grey', minHeight: '100px' }}>
+      <button type="button" onClick={() => openInPortal()}>
+        Open in portal
+      </button>
+    </div>
+  );
+};
+
+export default DeckHeader;
