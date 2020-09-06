4f0aa5571ff0f6792e1d80b4e25c33b0698c550e
Hatred <48438453+Heionbuji@users.noreply.github.com>
2020-09-06 17:31:29 +0300

    Allow i++ in for loops

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,5 +28,6 @@ module.exports = {
     'import/prefer-default-export': 0,
     'implicit-arrow-linebreak': 0,
     'react/jsx-one-expression-per-line': 0,
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
   },
 };
