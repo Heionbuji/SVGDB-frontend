e906e4886f96f39c97d1a0217ec02d8f89baf868
Hatred <48438453+Heionbuji@users.noreply.github.com>
2021-08-15 17:19:26 +0300

    Only update ad on route change

--- src/components/AdContainer.jsx
+++ src/components/AdContainer.jsx
@@ -1,21 +1,39 @@
-import React, { useEffect } from 'react';
+/* eslint-disable react/no-redundant-should-component-update */
+import React from 'react';
 
-const AdContainer = ({ currentPath }) => {
-  useEffect(() => {
-    (window.adsbygoogle = window.adsbygoogle || []).push({});
-  }, [currentPath]);
-  return (
-    <div key={currentPath}>
-      <ins
-        className="adsbygoogle"
-        style={{ display: 'block', margin: '5px auto' }}
-        data-ad-client="ca-pub-8451742260884006"
-        data-ad-slot="6068453122"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      />
-    </div>
-  );
+const initAd = () => {
+  (window.adsbygoogle = window.adsbygoogle || []).push({});
 };
+
+class AdContainer extends React.PureComponent {
+  componentDidMount() {
+    initAd();
+  }
+
+  shouldComponentUpdate(nextProps) {
+    const { key } = this.props.currentPath;
+    return nextProps.currentPath.key !== key;
+  }
+
+  componentDidUpdate() {
+    initAd();
+  }
+
+  render() {
+    const { key } = this.props.currentPath;
+    return (
+      <div key={key}>
+        <ins
+          className="adsbygoogle"
+          style={{ display: 'block', margin: '5px auto' }}
+          data-ad-client="ca-pub-8451742260884006"
+          data-ad-slot="6068453122"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        />
+      </div>
+    );
+  }
+}
 
 export default AdContainer;
